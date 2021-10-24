8e9af0e739f3df39db3814df0748a9ae90b4941b
Kirilll <vilyam13@gmail.com>
2021-10-24 15:27:55 +0300

    client_&_server

--- server/test/immutable_spec.js
+++ server/test/immutable_spec.js
@@ -1,74 +1,58 @@
-import {List, Map} from 'immutable';
-import {expect} from 'chai';
+import { List, Map } from 'immutable';
+import { expect } from 'chai';
 
 describe('immutability', () => {
+	describe('numbers', () => {
+		function increment(currentState) {
+			return currentState + 1;
+		}
 
-  describe('numbers', () => {
+		it('are immutable', () => {
+			let state = 42;
+			let nextState = increment(state);
 
-    function increment(currentState) {
-      return currentState + 1;
-    }
+			expect(nextState).to.equal(43);
+			expect(state).to.equal(42);
+		});
+	});
 
-    it('are immutable', () => {
-      let state = 42;
-      let nextState = increment(state);
+	describe('Lists', () => {
+		function addMovie(currentState, movie) {
+			return currentState.push(movie);
+		}
 
-      expect(nextState).to.equal(43);
-      expect(state).to.equal(42);
-    });
+		it('are immutable', () => {
+			let state = List.of('Trainspotting', '28 Days Later');
+			let nextState = addMovie(state, 'Sunshine');
 
-  });
+			expect(nextState).to.equal(
+				List.of('Trainspotting', '28 Days Later', 'Sunshine')
+			);
+			expect(state).to.equal(List.of('Trainspotting', '28 Days Later'));
+		});
+	});
 
-  describe('Lists', () => {
+	describe('trees', () => {
+		function addMovie(currentState, movie) {
+			return currentState.update('movies', (movies) => movies.push(movie));
+		}
 
-    function addMovie(currentState, movie) {
-      return currentState.push(movie);
-    }
+		it('are immutable', () => {
+			let state = Map({
+				movies: List.of('Trainspotting', '28 Days Later'),
+			});
+			let nextState = addMovie(state, 'Sunshine');
 
-    it('are immutable', () => {
-      let state = List.of('Trainspotting', '28 Days Later');
-      let nextState = addMovie(state, 'Sunshine');
-
-      expect(nextState).to.equal(List.of(
-        'Trainspotting',
-        '28 Days Later',
-        'Sunshine'
-      ));
-      expect(state).to.equal(List.of(
-        'Trainspotting',
-        '28 Days Later'
-      ));
-    });
-
-  });
-
-  describe('trees', () => {
-
-    function addMovie(currentState, movie) {
-      return currentState.update('movies', movies => movies.push(movie));
-    }
-
-    it('are immutable', () => {
-      let state = Map({
-        movies: List.of('Trainspotting', '28 Days Later')
-      });
-      let nextState = addMovie(state, 'Sunshine');
-
-      expect(nextState).to.equal(Map({
-        movies: List.of(
-          'Trainspotting',
-          '28 Days Later',
-          'Sunshine'
-        )
-      }));
-      expect(state).to.equal(Map({
-        movies: List.of(
-          'Trainspotting',
-          '28 Days Later'
-        )
-      }));
-    });
-
-  });
-
+			expect(nextState).to.equal(
+				Map({
+					movies: List.of('Trainspotting', '28 Days Later', 'Sunshine'),
+				})
+			);
+			expect(state).to.equal(
+				Map({
+					movies: List.of('Trainspotting', '28 Days Later'),
+				})
+			);
+		});
+	});
 });
--- server/utils/randNumArr.js
+++ server/utils/randNumArr.js
@@ -1,7 +1,7 @@
 export const randNumArr = () => {
-    let arr = [];
-    let firstNum = Math.floor(Math.random() * 6);
-    arr[0] = firstNum;
-    arr[1] = (firstNum & 1) ? --firstNum : ++firstNum;
-    return arr;
+	let arr = [];
+	let firstNum = Math.floor(Math.random() * 6);
+	arr[0] = firstNum;
+	arr[1] = firstNum & 1 ? --firstNum : ++firstNum;
+	return arr;
 };
